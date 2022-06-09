15e64c435f016c26e926f8c17630e41bf600b854
Albert Pichler <albertpichler@gmx.net>
2022-06-09 15:11:24 +0200

    Remove potential problematic font rule

--- pages/_app.js
+++ pages/_app.js
@@ -38,9 +38,6 @@ export default function ECommerce({ Component, pageProps }) {
           body {
             margin: 0;
             padding: 0;
-            font-family: Inter, -apple-system, BlinkMacSystemFont, Segoe UI,
-              Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans,
-              Helvetica Neue, sans-serif;
           }
           * {
             box-sizing: border-box;
